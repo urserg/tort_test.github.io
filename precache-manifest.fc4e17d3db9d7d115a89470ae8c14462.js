f04beb618ae3896adcb12f1ac5cdddc5bd6c625c
sergey <urserg@gmail.com>
2021-03-09 20:01:59 +0400

    Updates

---
+++ precache-manifest.fc4e17d3db9d7d115a89470ae8c14462.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d0f0ba85d372cefa63ed2acff8ec2ef",
+    "url": "/tort_test.github.io/index.html"
+  },
+  {
+    "revision": "4d18ab675bd4bde5c211",
+    "url": "/tort_test.github.io/static/css/main.3b402dae.chunk.css"
+  },
+  {
+    "revision": "619b42329376d28480d5",
+    "url": "/tort_test.github.io/static/js/2.864f3bb4.chunk.js"
+  },
+  {
+    "revision": "e25800ec0f990f7a13021008bb3d131c",
+    "url": "/tort_test.github.io/static/js/2.864f3bb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d18ab675bd4bde5c211",
+    "url": "/tort_test.github.io/static/js/main.2ea2ad72.chunk.js"
+  },
+  {
+    "revision": "9c6fb35d70cc7c18aadc",
+    "url": "/tort_test.github.io/static/js/runtime-main.f16a2ae3.js"
+  },
+  {
+    "revision": "2d4cd8722065da2ac700199273325752",
+    "url": "/tort_test.github.io/static/media/Raleway-Regular.2d4cd872.ttf"
+  },
+  {
+    "revision": "7e6eb475749b3edd88578f5ee18f2c4c",
+    "url": "/tort_test.github.io/static/media/Raleway-Regular.7e6eb475.woff"
+  },
+  {
+    "revision": "03492a956f396ce06cc0750ad5d0242d",
+    "url": "/tort_test.github.io/static/media/Recepies.03492a95.svg"
+  },
+  {
+    "revision": "7e684c663d1a7aee37c57974f6784688",
+    "url": "/tort_test.github.io/static/media/calendar_left.7e684c66.svg"
+  },
+  {
+    "revision": "4baaee01cc78fefb9dc99965f6cc2d21",
+    "url": "/tort_test.github.io/static/media/calendar_top_mob.4baaee01.svg"
+  },
+  {
+    "revision": "e2eff902525f897db997da92fffad3a0",
+    "url": "/tort_test.github.io/static/media/clock.e2eff902.svg"
+  },
+  {
+    "revision": "07c1ee97768c6b12664d2f2ffdffb586",
+    "url": "/tort_test.github.io/static/media/clock1.07c1ee97.svg"
+  },
+  {
+    "revision": "88f21b6448ddb7c8b310d8e5f5b328ac",
+    "url": "/tort_test.github.io/static/media/ingredients_left.88f21b64.svg"
+  },
+  {
+    "revision": "eedd0c22f45870628e643ef7e66fabea",
+    "url": "/tort_test.github.io/static/media/instagram_small.eedd0c22.svg"
+  },
+  {
+    "revision": "4e1ceba04818225b7d5fdc550ba6648b",
+    "url": "/tort_test.github.io/static/media/instrument1.4e1ceba0.svg"
+  },
+  {
+    "revision": "1c2660215b278d377d554e6cf361a9f4",
+    "url": "/tort_test.github.io/static/media/logo.1c266021.svg"
+  },
+  {
+    "revision": "9c3cf66a823b43de8f894bea7d3b9db8",
+    "url": "/tort_test.github.io/static/media/pasrtynet.9c3cf66a.svg"
+  },
+  {
+    "revision": "cb55560185b028287d8eb42d8baff8f7",
+    "url": "/tort_test.github.io/static/media/person.cb555601.svg"
+  },
+  {
+    "revision": "da9ac001e9b566208f614382aec1e9b7",
+    "url": "/tort_test.github.io/static/media/phone_small.da9ac001.svg"
+  },
+  {
+    "revision": "b35059bd74e23de18b897486dd084501",
+    "url": "/tort_test.github.io/static/media/signin.b35059bd.svg"
+  },
+  {
+    "revision": "9d67bb76b82a37e8dba0ac2d38021eb6",
+    "url": "/tort_test.github.io/static/media/sing_in_mobile.9d67bb76.svg"
+  },
+  {
+    "revision": "a5df07ce3f58a6dcfe07a882dd8121ba",
+    "url": "/tort_test.github.io/static/media/tort_item_img.a5df07ce.png"
+  },
+  {
+    "revision": "b5752a67505734d0af8c918424754295",
+    "url": "/tort_test.github.io/static/media/truck.b5752a67.svg"
+  },
+  {
+    "revision": "18927908bf062e1f919e368931fb7f06",
+    "url": "/tort_test.github.io/static/media/whatsapp_small.18927908.svg"
+  },
+  {
+    "revision": "1758ddef3e976ec6083d3bb6f5771902",
+    "url": "/tort_test.github.io/static/media/works.1758ddef.svg"
+  }
+]);
